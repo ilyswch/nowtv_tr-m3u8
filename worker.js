7685ba43e72e950e71f6d0b88bb26cd2ffce09be
ilyswch <116973025+ilyswch@users.noreply.github.com>
2024-05-13 13:07:14 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,32 @@
-
+addEventListener('fetch', (event) => {
+    event.respondWith(
+      handleRequest(event.request).catch(
+        (err) => new Response(err.message, { status: 500 })
+      )
+    )
+  })
+  
+  async function handleRequest(request) {
+    const { pathname } = new URL(request.url)
+  
+    if (pathname === '/live/index.m3u8') {
+      const url = `https://www.nowtv.com.tr/canli-yayin`
+  
+      const response = await fetch(url, {
+        cf: {
+          cacheTtl: 10800,
+          cacheEverything: true
+        }
+      })
+  
+      if (response.ok) {
+        const text = await response.text()
+        const stream = text.match(/(?<=daiUrl : ').*?(?=',)/g);
+  
+        return Response.redirect(stream, 302)
+      } else {
+        return new Response('Failed to fetch live stream URL', { status: 500 })
+      }
+    }
+  }
+  
